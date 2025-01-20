1adf502106efd4e2cd23bf8f4b6893ccdee1cba8
Candice Valdecanas <valdecanas.11@gmail.com>
2025-01-20 20:39:44 +0800

    feat: :sparkles: Added for routes

--- index.js
+++ index.js
@@ -41,6 +41,12 @@ app.listen(PORT, () => {
 }); */
 
 // Start the server Microsoft Azure
+
+// Catch-all handler for undefined routes
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
